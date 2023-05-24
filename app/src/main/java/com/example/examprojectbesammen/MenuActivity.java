50fc37bb632417197394a036e1e5d3c82905d7a4
daugbjerg <daugbjerglasse@gmail.com>
2023-05-24 16:51:25 +0200

    font (montserrat) installeret. knapper sat op efter design. Forsøg på at henvise til siderne og onClick

--- app/src/main/java/com/example/examprojectbesammen/MenuActivity.java
+++ app/src/main/java/com/example/examprojectbesammen/MenuActivity.java
@@ -1,15 +1,30 @@
 package com.example.examprojectbesammen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button tipsAndTricksMenuBtn = findViewById(R.id.tipsAndTricksMenuBtn);
+        tipsAndTricksMenuBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MenuActivity.this, TipsAndTricksActivity.class);
+            startActivity(intent);
+        });
+
+        // Set click listeners for other menu buttons here
+    }
+
+    public void onTipsAndTricksMenuButtonClick(View view) {
+        Intent intent = new Intent(MenuActivity.this, TipsAndTricksActivity.class);
+        startActivity(intent);
     }
 }
