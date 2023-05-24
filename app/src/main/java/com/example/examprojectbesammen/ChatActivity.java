e4f0b617a3d560a438b5d9f1871fcc652dd47ee2
daugbjerg <daugbjerglasse@gmail.com>
2023-05-24 17:48:36 +0200

    Activities added to manifest. Started on the chat funtion. Layout keeps crashing, fixes incoming.

---
+++ app/src/main/java/com/example/examprojectbesammen/ChatActivity.java
@@ -0,0 +1,52 @@
+package com.example.examprojectbesammen;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private FirebaseFirestore firestore;
+    private CollectionReference messagesRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
+        setContentView(R.layout.activity_chat);
+
+        firestore = FirebaseFirestore.getInstance();
+        messagesRef = firestore.collection("messages");
+    }
+
+    private void sendMessage(String message) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("text", message);
+
+        messagesRef.add(data)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        // Message sent successfully
+                        // Clear the input field or perform other actions
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Handle message sending failure
+                    }
+                });
+    }
+}
