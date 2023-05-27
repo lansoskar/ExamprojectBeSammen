b70c73b9cfd596b230753ec6ba2fb5548a413e40
davoquavowavo <david.saltorp@gmail.com>
2023-05-27 14:16:05 +0200

    Button redesign, colors dont show in layout but are hardcoded in class files

--- app/src/main/java/com/example/examprojectbesammen/MenuActivity.java
+++ app/src/main/java/com/example/examprojectbesammen/MenuActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -15,11 +16,21 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         Button tipsAndTricksMenuBtn = findViewById(R.id.tipsAndTricksMenuBtn);
+        tipsAndTricksMenuBtn.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.teal_500));
+
+        Button profileBtn = findViewById(R.id.profileMenuBtn);
+        profileBtn.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.teal_500));
+
+        Button yourConversations = findViewById(R.id.chatMenuBtn);
+        yourConversations.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.teal_700));
+
+
         tipsAndTricksMenuBtn.setOnClickListener(v -> {
             Intent intent = new Intent(MenuActivity.this, TipsAndTricksActivity.class);
             startActivity(intent);
         });
-        Button yourConversations = findViewById(R.id.chatMenuBtn);
+        yourConversations = findViewById(R.id.chatMenuBtn);
+
         yourConversations.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
